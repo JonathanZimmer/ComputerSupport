d7e1c62ec5eb8fbfee1d8070c92d073c5e2fba49
zimmerjonathan24 <zimmerjonathan24@gmail.com>
2017-08-11 08:39:32 -0400

    Going on break

--- SparesPage.php
+++ SparesPage.php
@@ -1,6 +1,19 @@
-<?php
-	require_once "config.php";
-	require_once "functions.php";
-	$conn = mysql_connect("localhost", $g_username, $g_password);
-	mysql_select_db('stt', $conn);
-?>
+<center>
+	<?php
+		require_once "config.php";
+		require_once "functions.php";
+		makeHeader("Spares","Spare Laptops",3,"SparesPage.php");
+		$conn = mysql_connect("localhost", $g_username, $g_password);
+		mysql_select_db('stt', $conn);
+		$None="";
+		$CountOut= mysql_query("SELECT COUNT(*) FROM `Spares` WHERE `Student` != '' ");
+		if (mysql_result($CountOut, 0, 0) > 0) {
+			$Out=mysql_query("SELECT * FROM Spares WHERE Students != '' ");
+		}
+		else{
+			echo "No spares are out<br>";
+			echo $CountOut;
+			
+		}
+	?>
+</center>
--- functions.php
+++ functions.php
@@ -16,52 +16,13 @@ function makeHeader($tbtitle,$ptitle,$pstyle,$fileName, $hhtml=""){
 		</head>
 		<body onload='calculateStuff();'>
 			<div class='header'>
-				<a href='../index.php'><h1 id='headerTitle'>". $ptitle . "</h1></a>
-			
+				<h1 id='headerTitle'>". $ptitle . "</h1>
 				");
  
 
 
 }
-//footer function to set up the end of a webpage easier
-//call this function by declaring
-//makeFooter(String: "message", Int style#);
-//leave style as 0 if you want to keep the style from the makeHeader functions
-function makeFooter($footermessage,$pstyle,$footerbox=""){
-	echo("
-	</div>
-		<style>". styleChoice($pstyle) ."</style>
-			");
-			 if ($footerbox == "False"){
-			 }else{
-				echo ("
-				<div id='footer'>
-					<p id='footerMessage'>". $footermessage . "</p>
-				</div>
-				");
-			 }
-			 echo ("
-		<body>
-	<html>
-	<script>
-	
-	function calculateStuff(){
-	
-		var body = document.body, html = document.documentElement;
-		
-		var footer = document.getElementById('footer');
-		var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight ) + 25;
-		
-		body.style.height = height;
-		
-		footer.style.width = '78%';
-		footer.style.height= '25px';
-		footer.style.top = height - 25;
-	}
-	
-	</script>
-	");
-}
+
 //stores desired theme into a variable
 function styleChoice($styleNum){
 	switch($styleNum){
@@ -72,7 +33,7 @@ function styleChoice($styleNum){
 		return $styleText = '<link href="/css_files/headerStyle2.css" rel="stylesheet">';
 		break;
 		case 3://braves theme
-		return $styleText = '<link href="/css_files/headerStyle3.css" rel="stylesheet";>';
+		return $styleText = '<link href="/css_files/Normal_Tables.css" rel="stylesheet";>';
 		break;
 		default://select this style if you want to have your own style. Just have your <style> or <link> tags be your first line of code between the header and footer functions
 		return $styleText = "";
