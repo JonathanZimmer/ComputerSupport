b6d57b54753a9e565dc52808ae92f3f4c648ab77
zimmerjonathan24 <zimmerjonathan24@gmail.com>
2017-08-17 08:50:16 -0400

    just doing more

--- database_schema.sql
+++ database_schema.sql
@@ -1,34 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Sep 10, 2015 at 01:54 PM
--- Server version: 5.5.38-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
- 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
- 
- 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
- 
---
--- Database: `stt`
---
+
+
 CREATE DATABASE IF NOT EXISTS `stt` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `stt`;
- 
- 
------------------------------------------------------------
- 
---
--- Table structure for table `incidents`
---
- 
+
+
 DROP TABLE IF EXISTS `LaptopHistory`;
 CREATE TABLE IF NOT EXISTS `LaptopHistory` (
   `id` int(3) NOT NULL AUTO_INCREMENT,
@@ -53,12 +30,6 @@ CREATE TABLE IF NOT EXISTS `LaptopHistory` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
 
------------------------------------------------------------
- 
---
--- Table structure for table `students`
---
- 
 DROP TABLE IF EXISTS `students`;
 CREATE TABLE IF NOT EXISTS `students` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -69,21 +40,8 @@ CREATE TABLE IF NOT EXISTS `students` (
   `admin` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
- 
---
--- Dumping data for table `students`
---
- 
-INSERT INTO `students` (`id`, `username`, `password`, `name`, `active`, `admin`) VALUES
-(1, 'admin', '$2a$10$E0hKDlY5A7HrmatD9vcSMeO8opsr.C.ZVV5j9fxCrldmq/TZGmbZC', 'Admin', 1, 1);
- 
- 
------------------------------------------------------------
- 
---
--- Table structure for table `inventory`
---
- 
+
+
 DROP TABLE IF EXISTS `inventory`;
 CREATE TABLE IF NOT EXISTS `inventory` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -94,7 +52,7 @@ CREATE TABLE IF NOT EXISTS `inventory` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
- 
+
 DROP TABLE IF EXISTS `Spares`;
 CREATE TABLE IF NOT EXISTS `Spares` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -104,8 +62,6 @@ CREATE TABLE IF NOT EXISTS `Spares` (
   PRIMARY KEY (`id`))
   ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
 
------------------------------------------------------------
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
- 
+
+INSERT INTO `students` (`id`, `username`, `password`, `name`, `active`, `admin`) VALUES
+(1, 'admin', '$2a$10$E0hKDlY5A7HrmatD9vcSMeO8opsr.C.ZVV5j9fxCrldmq/TZGmbZC', 'Admin', 1, 1);
